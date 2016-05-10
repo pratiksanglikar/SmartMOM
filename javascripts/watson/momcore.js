e81f61667ffd24492a742e65e763625b8bf1d8de
Pratik Sanglikar <pratik.sanglikar@sjsu.edu>
2016-05-10 08:42:40 -0700

    file saving using MongoDB done.

---
+++ javascripts/watson/momcore.js
@@ -0,0 +1,63 @@
+/**
+ * Created by pratiksanglikar on 10/05/16.
+ */
+
+var MongoDB = require("../commons/mongodbhandler");
+var Q = require("q");
+
+exports.addNewFile = function (username, filename, timestamp) {
+	var deferred = Q.defer();
+	MongoDB.collection("files").insert({
+		author: username,
+		filename: filename,
+		created_on: timestamp
+	}).then(function () {
+		deferred.resolve(filename);
+	}).catch(function (error) {
+		deferred.reject(error);
+	});
+	return deferred.promise;
+}
+
+exports.getFilesByUserName = function (username) {
+	var deferred = Q.defer();
+	var files = [];
+	var cursor = MongoDB.collection("files").find({
+		author: username
+	});
+	cursor.each( function (error, doc) {
+		if(error){
+			deferred.reject(error);
+		}
+		if(doc != null) {
+			files.push(doc);
+		} else {
+			deferred.resolve(files);
+		}
+	});
+	return deferred.promise;
+}
+
+exports.getMOMByFileName = function (filename) {
+	var deferred = Q.defer();
+	var mom = null;
+	var cursor = MongoDB.collection("mom").find({
+		filename: filename
+	});
+	cursor.each(function (error, doc) {
+		if(error) {
+			deferred.reject(error);
+		} else {
+			if(doc != null) {
+				mom = doc;
+			} else {
+				if(mom) {
+					deferred.resolve(mom);
+				} else {
+					deferred.reject("Minutes of meeting not found for " + filename);
+				}
+			}
+		}
+	});
+	return deferred.promise;
+}
