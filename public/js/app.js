9a752edd051431ef337f16c5ba311cc920668772
Pratik Sanglikar <pratik.sanglikar@sjsu.edu>
2016-05-10 14:39:47 -0700

    changed the audio recording method to 'wav' from 'mp3'

--- public/js/app.js
+++ public/js/app.js
@@ -11,5 +11,5 @@ app.config(["$routeProvider","$locationProvider","recorderServiceProvider",funct
 		redirectTo: "/record"
 	});
 	$locationProvider.html5Mode(true);
-	RecorderServiceProvider.withMp3Conversion(true);
+	RecorderServiceProvider.withMp3Conversion(false);
 }]);
